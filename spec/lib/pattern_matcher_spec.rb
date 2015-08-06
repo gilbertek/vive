752c3fd33b9bb0c2a0b8f741e77b067a17ffdbd2
Gilbert F. Sewovoe-Ekoue <gilbert.fsw@therealreal.com>
2015-08-05 20:36:58 -0400

    amend

--- spec/lib/pattern_matcher_spec.rb
+++ spec/lib/pattern_matcher_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+require './lib/pattern_matcher'
+
+RSpec.describe PatternMatcher do
+  let(:pattern) { "foo,bar,baz" }
+  let(:path) { "foo/bar/baz/" }
+
+end
--- spec/lib/pattern_spec.rb
+++ spec/lib/pattern_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+require './lib/pattern'
+
+RSpec.describe Pattern do
+  let(:pattern) { "foo,bar,baz" }
+  it "should have a length greater than 1" do
+    expect(pattern.length).to satisfy("be greater than 1") { |v| v > 1 }
+  end
+end
